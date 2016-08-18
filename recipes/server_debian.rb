ce443fc58c0b1835d743e8d963d234b77d4e4204
Mariusz Ołownia <ollownia@gmail.com>
2016-08-18 12:32:58 +0200

    debian server fix

--- recipes/server_debian.rb
+++ recipes/server_debian.rb
@@ -29,8 +29,8 @@ service "postgresql" do
   action [:enable, :start]
 end
 
-execute 'Set locale and Create cluster' do
-  command 'export LC_ALL=C; /usr/bin/pg_createcluster --start ' + node['postgresql']['version'] + ' main'
-  action :run
-  not_if { ::File.directory?('/etc/postgresql/' + node['postgresql']['version'] + '/main') }
-end
+# execute 'Set locale and Create cluster' do
+#   command 'export LC_ALL=C; /usr/bin/pg_createcluster --start ' + node['postgresql']['version'] + ' main'
+#   action :run
+#   not_if { ::File.directory?('/etc/postgresql/' + node['postgresql']['version'] + '/main') }
+# end
